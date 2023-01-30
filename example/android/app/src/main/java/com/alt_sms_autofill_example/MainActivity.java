4ac650c6419efecf3e1706733e73fbd7fa9b7503
Hyunwoo Song <luciris99@gmail.com>
2023-01-31 02:19:23 +0900

    change FlutterActivity -> FlutterFragmentActivity

--- example/android/app/src/main/java/com/alt_sms_autofill_example/MainActivity.java
+++ example/android/app/src/main/java/com/alt_sms_autofill_example/MainActivity.java
@@ -1,6 +1,6 @@
 package com.alt_sms_autofill_example;
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterFragmentActivity {
 }
